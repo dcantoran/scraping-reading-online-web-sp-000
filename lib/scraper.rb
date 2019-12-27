922f2b12b0c485d271d7baf50ac7ebbd6f5a173b
David Cantoran <davidcantoran36@gmail.com>
2019-12-27 23:14:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,6 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 
-# puts doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid-3jXtmC")
+puts doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
-puts doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+# puts doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
