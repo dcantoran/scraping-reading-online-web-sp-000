af0a2c3b4caa3d8bd3c6f1b881eb763dff2412ef
David Cantoran <davidcantoran36@gmail.com>
2019-12-27 23:31:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,6 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
 courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
-puts courses[0].attributes
+puts courses[0].children
 
 # courses.each {|course| puts course.text.strip }
